8d40ab53bc8a1571c2933d45e55a19b8999d0df0
faizullah <faizullah@gmail.com>
2025-01-30 00:53:09 +0500

    Change db use env

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
-// const mongoURI = 'mongodb+srv://FaizullahBalghari:Faiz2003@cluster0.ncezhh1.mongodb.net/shoeStore?retryWrites=true&w=majority'
 const connectToMongo=()=>{
     mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -14,5 +13,4 @@ const connectToMongo=()=>{
 }
 mongoose.set('strictQuery', false);
 module.exports= connectToMongo;
-// "mongodb+srv://<username>:<password>@cluster0.ncezhh1.mongodb.net/inotebookDB?retryWrites=true&w=majority"
 
